b8e121be75767fabc8378645171ba694b7598f76
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-03-07 23:05:29 +1100

    2.11

--- Lab02/2.11.cpp
+++ Lab02/2.11.cpp
@@ -5,11 +5,13 @@ using namespace std;
 
 int main()
 {
-    double x,y,result=0;
+    double x,y,voltage=0;
+    int stage = 0;
     cout<<"Enter the frequency: \n";
     cin>> x;
-    
-    result = (275 / sqrt(pow(23,2) + 0.5 * pow(x,2))) *  4;
-    cout<<result;
+    cout<<"Enter the stage of the amplifier: \n";
+    cin>> stage;
+    voltage = pow((275 / sqrt(pow(23,2) + 0.5 * pow(x,2))),stage);
+    cout<< "At a frequency of " << x <<" hertz, the voltage gain is " <<voltage;
     return 0;
 }
