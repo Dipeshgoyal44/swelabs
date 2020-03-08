875bd6e44010741e3e8a7cbc54149e43d3f8985a
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-03-08 19:06:41 +1100

    comments

--- Lab02/2.11.cpp
+++ Lab02/2.11.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 int main()
 {
-    double x,y,voltage=0;
+    double x,y,voltage=0; // Variable Decalaration
     int stage = 0;
-    cout<<"Enter the frequency: \n";
+    cout<<"Enter the frequency: \n"; // User Prompts
     cin>> x;
     cout<<"Enter the stage of the amplifier: \n";
     cin>> stage;
-    voltage = pow((275 / sqrt(pow(23,2) + 0.5 * pow(x,2))),stage);
-    cout<< "At a frequency of " << x <<" hertz, the voltage gain is " <<voltage;
+    voltage = pow((275 / sqrt(pow(23,2) + 0.5 * pow(x,2))),stage); //Calculation
+    cout<< "At a frequency of " << x <<" hertz, the voltage gain is " <<voltage; //Output
     return 0;
 }
