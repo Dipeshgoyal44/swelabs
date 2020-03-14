b525e980b6a0f073ac5b6e9e3a5e223b64abb81b
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-03-14 14:59:25 +1100

    4.8

--- Lab04/4.8.cpp
+++ Lab04/4.8.cpp
@@ -1,72 +1,66 @@
 # include <iostream>
 using namespace std;
 
-
-int add(int a, int b)
-{
-    int add;
-    add = a + b;
-
-    // Return statement
-    return add;
-}
-
-int subtract(int a, int b)
-{
-    int subtract;
-    subtract = a - b;
-
-    // Return statement
-    return subtract;
-}
-
-int divide(int a, int b)
-{
-    int divide;
-    divide = a / b;
-
-    // Return statement
-    return divide;
-}
-
-
+void add(double a, double b);
+void subtract(double a, double b);
+void divide(double a, double b);
 
 int main()
 {
     int op;
     double num1, num2;
-    
-    cout << "Enter The First Number:\n";
+    do{  
+    cout << "\nEnter The First Number:\n";
     cin >> num1;
     cout << "Enter The Second Number:\n";
     cin >> num2;
 
-    cout << "Choose your Operator 1(Add), 2(Subtract), 3(Divide), 4(Exit)";
+    cout << "Choose your Operator 1(Add), 2(Subtract), 3(Divide), 4(Exit) \n";
     cin >> op;
 
     switch(op)
     {
-        case '1':
+        case 1:
             add(num1,num2);
             break;
 
-        case '2':
+        case 2:
             subtract(num1,num2);
             break;
 
-        case '3':
+        case 3:
             divide(num1,num2);
             break;
 
-        case '4':
-            cout << "The Program will now Exit \n";
+        case 4:
+            cout << "The Program will now exit \n";
             return 0;
             break;
 
         default:
             cout << "Error! Invalid Selection";
             break;
-    }
+    }}while(op != 4);
+}
 
-    return 0;
+
+void add(double a, double b)
+{
+    double add;
+    add = a + b;
+    cout << "The result is: " << add << "\n" ;
+}
+
+void subtract(double a, double b)
+{
+    double subtract;
+    subtract = a - b;
+    cout << "The result is: " << subtract << "\n" ;
+}
+
+void divide(double a, double b)
+{
+    double divide;
+    divide = a / b;
+    cout << "The result is: " << divide << "\n" ;
 }
