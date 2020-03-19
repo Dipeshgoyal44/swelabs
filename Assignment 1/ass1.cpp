2a32af66cb1f482a4395243e7cd4c59517aa6522
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-03-19 16:49:11 +1100

    encoding message

--- Assignment 1/ass1.cpp
+++ Assignment 1/ass1.cpp
@@ -1,89 +1,94 @@
-#include <iostream>
-using namespace std;
-
-void reverse_array(int array[], int arraylength);
-
-int main()
-{
-    int number = 0;
-    int length = 0;
-    int temp = 0;
-
-    cout << "Enter The Number: \n";
-    cin >> number;
-    length = to_string(number).length();
-    int num[length];
-    for (int i = length - 1; i >= 0; i--)
+    #include <iostream>
+    using namespace std;
+    
+    void reverse_array(int array[], int arraylength);
+    
+    int main()
     {
-        num[i] = number % 10;
-        number /= 10;
-    }
-
-    switch (length)
-    {
-    case 2:
-        cout << "\n Case 1:\n";
+        int number = 0;
+        int length = 0;
+        int temp = 0;
+    
+        cout << "Enter The Number: \n";
+        cin >> number;
+        length = to_string(number).length();
+        int num[length];
+        cout << "Number contains "<< length << " digits\n";
         for (int i = length - 1; i >= 0; i--)
         {
-            cout << num[i];
+            num[i] = number % 10;
+            number /= 10;
         }
-        break;
-    case 3:
-        cout << "Case 2\n";
+    
+        switch (length)
+        {
+        case 2:
+            for (int i = length - 1; i >= 0; i--)
+            {
+                cout << "The encoded number is " << num[i];
+            }
+            break;
+        case 3:
+            reverse_array(num, length);
+            temp = num[0];
+            num[0] = num[2];
+            num[2] = temp;
+            cout << "The encoded number is ";
+            for (int i = length - 1; i >= 0; i--)
+            {
+                cout <<  num[i];
+            }
+            break;
+        case 4:
         reverse_array(num, length);
-        temp = num[0];
-        num[0] = num[2];
-        num[2] = temp;
-        for (int i = length - 1; i >= 0; i--)
-        {
-            cout << num[i];
-        }
-        break;
-    case 4:
+        cout << "The encoded number is ";
+            for (int i = length; i >= 1; i--)
+            {
+                cout << (num[i-1]+i) % 10;
+            }
+            break;
+        case 5:
+        reverse_array(num, length);
+        cout << "The encoded number is ";
         for (int i = length; i >= 1; i--)
-        {
-            cout << (num[i-1]+i) % 10 << endl;
+            {
+                cout << (num[i-1]+i) % 10;
+            }
+            break;
+        case 6:
+        reverse_array(num, length);
+        cout << "The encoded number is ";
+        for (int i = length; i >= 1; i--)
+            {
+                cout << (num[i-1]+i) % 10;
+            }
+            break;
+        case 7:
+        reverse_array(num, length);
+        cout << "The encoded number is ";
+        for (int i = length; i >= 1; i--)
+            {
+                cout << (num[i-1]+i) % 10;
+            }
+            break;
+        case 8:
+        reverse_array(num, length);
+        cout << "The encoded number is ";
+        for (int i = length; i >= 1; i--)
+            {
+                cout << (num[i-1]+i) % 10;
+            }
+            break;
         }
-        cout << "\nCase 3\n";
-        break;
-    case 5:
-    for (int i = length; i >= 1; i--)
-        {
-            cout << (num[i-1]+i) % 10 << endl;
-        }
-        cout << "Case 3\n";
-        break;
-    case 6:
-    for (int i = length; i >= 1; i--)
-        {
-            cout << (num[i-1]+i) % 10 << endl;
-        }
-        cout << "Case 3\n";
-        break;
-    case 7:
-    for (int i = length; i >= 1; i--)
-        {
-            cout << (num[i-1]+i) % 10 << endl;
-        }
-        cout << "Case 3\n";
-        break;
-    case 8:
-    for (int i = length; i >= 1; i--)
-        {
-            cout << (num[i-1]+i) % 10 << endl;
-        }
-        cout << "Case 3\n";
-        break;
+        return 0;
     }
-    return 0;
-}
-
-void reverse_array(int array[], int arraylength)
-{
-    for (int i = 0; i < (arraylength / 2); i++)
+    
+    void reverse_array(int array[], int arraylength)
     {
-        int temporary = array[i]; // temporary wasn't declared
-        array[i] = array[(arraylength - 1) - i];
-        array[(arraylength - 1) - i] = temporary;
+        for (int i = 0; i < (arraylength / 2); i++)
+        {
+            int temporary = array[i]; // temporary wasn't declared
+            array[i] = array[(arraylength - 1) - i];
+            array[(arraylength - 1) - i] = temporary;
+        }
     }
-}
