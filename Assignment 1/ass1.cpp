8123b0c8f8eac004f43e9123ef0dc737545a85ef
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-03-26 19:10:43 +1100

    lab5

--- Assignment 1/ass1.cpp
+++ Assignment 1/ass1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// Function Prototype
+// Function Prototypes
 void reverse_array(int array[], int arraylength);
 void decode(int array[], int arraylength);
 
@@ -12,7 +12,7 @@ int main()
     int length = 0;
     int calc = 0;
     int j;
-    
+
     //Do while loop for validation of input
     do
     {
@@ -24,8 +24,8 @@ int main()
         }
     } while (number < 10 || number > 99999999); //while condition
 
-    length = to_string(number).length(); //convering the input into string to check no. of digits and storing in length
-    int num[length]; // using the length for array size.
+    length = to_string(number).length();                 //convering the input into string to check no. of digits and storing in length
+    int num[length];                                     // using the length for array size.
     cout << "Number contains " << length << " digits\n"; //Display how many digits in the input
 
     for (int i = length - 1; i >= 0; i--) // Loop to store the input entered into the array
@@ -38,26 +38,26 @@ int main()
     switch (length) // Switch statement based on the length of the input
     {
     case 2:
-        cout << "The encoded number is "; //Encoded Number
+        cout << "The encoded number is ";     //Encoded Number
         for (int i = length - 1; i >= 0; i--) // Backwards loop to  to encode the input
         {
             cout << num[i];
         }
-        reverse_array(num, length); // reversing the encoded array to decode
-        cout << "\nThe decoded number is "; //Decoded Number
+        reverse_array(num, length);           // reversing the encoded array to decode
+        cout << "\nThe decoded number is ";   //Decoded Number
         for (int i = length - 1; i >= 0; i--) // Loop to display the decoded output
         {
             cout << num[i];
         }
         break;
     case 3:
-        cout << "The encoded number is "; //Encoded Number
+        cout << "The encoded number is ";     //Encoded Number
         for (int i = length - 1; i >= 0; i--) // Backwards loop to  to encode the input
         {
             cout << num[i];
         }
-        reverse_array(num, length); // reversing the encoded array to decode
-        cout << "\nThe decoded number is "; //Decoded Number
+        reverse_array(num, length);           // reversing the encoded array to decode
+        cout << "\nThe decoded number is ";   //Decoded Number
         for (int i = length - 1; i >= 0; i--) // Loop to display the decoded output
         {
             cout << num[i];
@@ -140,55 +140,59 @@ int main()
         }
         break;
     case 8:
-    string choice; // variable for extra protection
+        string choice;                                                               // variable for extra protection
         cout << "Do you want to do a second level of encryption, enter yes or no? "; // prompt
         cin >> choice;
-        if (choice=="yes"){ //condition if yes to prompt
-        reverse_array(num, length);
-        cout << "The encoded number is ";
-        for (int i = length; i >= 1; i--)
-        {
-            num[i-1] = (num[i - 1] + i) % 10; //extra protection encoding
+        if (choice == "yes")
+        { //condition if yes to prompt
+            reverse_array(num, length);
+            cout << "The encoded number is ";
+            for (int i = length; i >= 1; i--)
+            {
+                num[i - 1] = (num[i - 1] + i) % 10; //extra protection encoding
+            }
+            for (int i = 0; i < length; i++)
+            {
+                cout << num[i];
+            }
+            cout << "\nThe decoded number is ";
+            decode(num, length);
+            reverse_array(num, length);
+            for (int i = 0; i < length; i++)
+            {
+                cout << num[i];
+            }
+            //WORKING!
         }
-        for (int i = 0; i < length; i++)
-        {
-            cout << num[i];
+        else
+        { // condition if no to the prompt
+            reverse_array(num, length);
+            cout << "The encoded number is ";
+            for (int i = length; i >= 1; i--)
+            {
+                calc = (num[i - 1] + i) % 10;
+                array[j] = calc;
+                j++;
+                cout << calc;
+            }
+            cout << "\nThe decoded number is ";
+            reverse_array(array, length);
+            decode(array, length);
+            reverse_array(array, length);
+            for (int i = 0; i < length; i++)
+            {
+                cout << array[i];
+            }
+            break;
         }
-        cout << "\nThe decoded number is ";
-        decode(num, length);
-        reverse_array(num, length);
-        for (int i = 0; i < length; i++)
-        {
-            cout << num[i];
-        }
-        //WORKING!
-        } else {  // condition if no to the prompt
-        reverse_array(num, length);
-        cout << "The encoded number is ";
-        for (int i = length; i >= 1; i--)
-        {
-            calc = (num[i - 1] + i) % 10;
-            array[j] = calc;
-            j++;
-            cout << calc;
-        }
-        cout << "\nThe decoded number is ";
-        reverse_array(array, length);
-        decode(array, length);
-        reverse_array(array, length);
-        for (int i = 0; i < length; i++)
-        {
-            cout << array[i];
-        }
-        break;
-    }}
+    }
     return 0;
 }
 void decode(int array[], int arraylength) // function to decode taken from video
 {
     for (int i = 0; i < arraylength; i++)
     {
-        array[i] += 9 - i; 
+        array[i] += 9 - i;
         array[i] = array[i] % 10;
     }
 }
@@ -196,7 +200,7 @@ void reverse_array(int array[], int arraylength) // Function to reverse arrays t
 {
     for (int i = 0; i < (arraylength / 2); i++)
     {
-        int temporary = array[i]; 
+        int temporary = array[i];
         array[i] = array[(arraylength - 1) - i];
         array[(arraylength - 1) - i] = temporary;
     }
