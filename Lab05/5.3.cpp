bdfeee7f467a6268510cba5bceec33ae8b99a37d
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-04-01 17:37:08 +1100

    5.3

--- Lab05/5.3.cpp
+++ Lab05/5.3.cpp
@@ -4,22 +4,23 @@
 using namespace std;
 
 int random(int num);
-
+void comment(int attempts);
 
 int main()
 {
-    string choice;
-    do
+    bool playAgain = true;
+    char choice;
+    while(!playAgain);
     {
     int num = random(num);
-	int guess, tries = 0;
+	int guess, attempts = 0;
 	cout << "I have a number between 1 and 1000.\n";
     cout << "Can you guess my number?\n";
 	do
 	{
 		cout << "Enter a guess between 1 and 1000 : ";
 		cin >> guess;
-		tries++;
+		attempts++;
 
 		if (guess > num)
 			cout << "Too high. Try again\n";
@@ -27,12 +28,17 @@ int main()
 			cout << "Too low. Try again\n";
 		else
 			cout << "\nExcellent! You guessed the number!\n";
+            comment(attempts);
             cout << "Would you like to play again?\n";
-            cout << "Please type yes or no?\n";
+            cout << "Please type y or n?\n";
             cin >> choice;
+            choice = toupper(choice);
+            if (choice== 'Y'){
+                playAgain = true;
+            }
             // << tries << " guesses!\n";
 	} while (guess != num);
-    }while (choice !="no");
+    }
 
 	return 0;
 }
@@ -42,4 +48,17 @@ int random(int num)
     srand(time(0)); //seed random number generator
 	num = rand() % 1000 + 1; // random number between 1 and 100
     return num;
+}
+
+void comment(int attempts)
+{
+    if(attempts <= 10){
+        cout << "Either you know the secret or you got lucky!\n"; 
+    }
+    if(attempts == 10){
+        cout << "Ahah! You knowthe secret!\n"; 
+    }
+    if(attempts >= 10){
+        cout << "You should be able to do better!\n";
+    }
 }
