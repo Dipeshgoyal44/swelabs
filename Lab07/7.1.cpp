625254ecd4e49f46a65432deed125e0db5fe0fe2
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-04-19 19:16:30 +1000

    7.1

--- Lab07/7.1.cpp
+++ Lab07/7.1.cpp
@@ -1,28 +1,46 @@
 #include <iostream>
 #include <fstream> // file open header file
+#include <cstdio> // c file 
 
 using namespace std;
 
+
 int main()
-{
-    int count, stor[100];
-    char filename[10]={"price.dat"};
-    readFile(filename,stor, count);
-    show(count,, stor);
-    system("pause");
-    return 0 ;
-}
 
-void readFile(char Price[], int storage [], int &noP)
 {
-    ifstream take(Price);
-    take >> noP;
-    for (int i = 0; i < noP; i++)
+    FILE *fp;
+    int count, i;
+    int a, total = 0, max = 0, min = 1000000, average = 0;
+    double b;
+
+    fp = fopen("price.dat", "r");
+    if (fp == NULL)
     {
-        take >> storage[i];
+        cout << " Error in opening the file....\n";
+        return 0;
     }
+    fscanf(fp, "%d", &count);
+    
+    for (i = 0; i < 10; i++)
+    {
+        fscanf(fp, "%d%f", &a, &b);
+        printf("%d           %.2f \n", a, b);
+        total = total + b;
+        if (max < b)
+            max = b;
+        if (min > b)
+            min = b;
+    }
+   
+    // printf("The total is = %d \n", total);
+    // printf("The average is = %d \n", total / count);
+    // printf("The max is =%d \n", max);
+    // printf("The min is =%d \n", min);
+//  fp = fopen("output.txt", "a");
+    // fprintf(fp, "The total is = %d \n", total);
+    // fprintf(fp, "The average is = %d \n", total / count);
+    // fprintf(fp, "The max is =%d \n", max);
+    // fprintf(fp, "The min is =%d \n", min);
 
-
-
-
+    return 0;
 }
