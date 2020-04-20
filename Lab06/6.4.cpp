01dd5852a9c3a4e3b10f14313270ac3b00106cd1
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-04-20 16:26:13 +1000

    6.4

---
+++ Lab06/6.4.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <ctime>
+
+using namespace std;
+
+
+int Random();
+
+int main() 
+{			
+	int dice1;	
+	int dice2;
+	const int columnSize = 6;
+	const int rowSize = 6;
+	int faces[columnSize][rowSize];
+	for (int row = 0; row < 6; row++)
+	{
+		for (int column = 0; column < 6; column++)
+		faces[row][column] = 0;
+	}
+	for (int i = 0; i < 50000; i++) 
+	{
+		dice1 = Random();
+		dice2 = Random();
+	
+	faces[dice1][dice2]++;
+	faces[6][6] = faces[dice1][dice2];
+	}
+	for(int row = 0; row < 6; row++)
+	{
+		cout<< "\n\t-----\t-----\t-----\t-----\t-----\t-----";
+        cout<<endl;
+		for(int column = 0; column < 6; column++)
+		{
+			cout<< "\t" << faces[column][row];
+            
+		}
+        cout<< "\n\t-----\t-----\t-----\t-----\t-----\t-----";
+		cout<<endl;
+	}
+
+	return 0;
+
+}
+
+
+int Random()
+{
+    int number;
+    static int flag = 1; //static so only meets the condition once.
+
+    if (flag == 1) // this ensures the function gives different random numbers everytime.
+    {
+        srand(time(0)); 
+        flag = 0;       
+    }
+    number = rand() % 6;
+    return number;
+}
