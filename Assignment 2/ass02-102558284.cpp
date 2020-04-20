6091b8a7a2ba3234a88e45d624039757ea5535d2
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-04-21 01:41:59 +1000

    ass02

---
+++ Assignment 2/ass02-102558284.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int total1(int array[],int arraySize);
+int total2(int array[],int arraySize);
+
+int main()
+{
+    int array[20];
+    int i=0,temp=0,count=0;
+    int sumTotal,checksum;
+
+    printf("Enter The Credit Card Number Below: \n");
+    for (i = 0; i < 20; i++)
+    {
+        scanf("%d", &temp);
+        if (temp != -1) // stores till -1 is entered
+        {
+            array[i] = temp;
+            count++;
+        }
+        else
+            break;
+    }
+    printf("The Credit Card number is: ");
+    int c;
+    for (c = 0; c < count; c++)
+    {
+        cout <<array[c];
+    }
+    printf("\n");
+
+// SUM 1
+    int sum1 = total1(array,count);
+    printf("Sum 1 is %d \n", sum1);
+
+    // SUM 2 
+    int sum2 = total2(array,count);
+    printf("Sum 2 is %d \n", sum2);
+
+    // CheckSum
+    sumTotal = sum1 + sum2;
+    sumTotal = sumTotal * 9;
+    checksum = sumTotal % 10;
+
+    printf("Check sum is %d \n", checksum);
+    printf("The last digit of the credit card is %d \n", array[count - 1]);
+
+    if (array[count - 1] == checksum)
+    {
+        printf("Check sum digit %d and the last digit %d are the same: Valid credit card number. \n", checksum, array[count - 1]);
+    }
+    else
+    {
+        printf("Check sum digit %d and the last digit %d are not the same: Invalid credit card number. \n", checksum, array[count - 1]);
+    }
+    return 0;
+}
+
+int total1(int array[],int arraySize)
+{
+    int total1,i,x=0,sum;
+    //int numberOne[20];
+    for (i = arraySize - 2; i >= 0; i = i - 2)
+    {
+        x = array[i] * 2;
+
+        if (x > 9)
+        {
+
+            x = x % 10;
+            sum = 1 + x;
+            total1 = total1 + sum;
+        }
+        else
+        {
+            total1 += array[i] * 2;
+        }
+    }
+    return total1;
+
+}
+
+int total2(int array[],int arraySize)
+{
+    int total2,i;
+
+    for(i = arraySize - 3; i >= 0; i = i - 2)
+    {
+        total2 = total2 + array[i];
+    }
+    return total2;
+    
+}
