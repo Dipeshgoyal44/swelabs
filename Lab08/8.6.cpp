54c4690b6184a62e67e4d830379575a1c7a63686
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-04-26 02:04:52 +1000

    8.6.cpp

---
+++ Lab08/8.6.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+
+void reorder(int *a , int *b, int *c);
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+
+    return 0;
+
+}
+
+void reorder(int *a , int *b, int *c)
+{
+    int temp = 0;
+    if( *a >  *b){ // a bigger than b
+    temp = *b; // a stored in temp bigger than b
+    *b = *a;
+    *a = temp;
+    }
+    if( *b > *c){
+    temp = *c; // a stored in temp bigger than b
+    *c = *b;
+    *b = temp;
+    }
+     if( *a > *c){
+    temp = *a; // a stored in temp bigger than b
+    *a = *c;
+    *c = temp;
+    }
+
+}
