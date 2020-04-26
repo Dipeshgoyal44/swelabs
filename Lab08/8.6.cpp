8af4ced828306384c60fa7a8dfe4d3a3af15190b
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-04-26 17:56:43 +1000

    8.6.cpp

--- Lab08/8.6.cpp
+++ Lab08/8.6.cpp
@@ -6,19 +6,19 @@ int calcVolts(int *current, int *resistance, int *voltage);
 void dispVolts(int *current, int *resistance, int *voltage);
 int main()
 {
-    int current[10] = {2,5,10,9,3,6,2,2,6,8};
-    int resistance[10] = {8,1,5,9,5,6,8,2,3,7};
+    int current[10] = {2, 5, 10, 9, 3, 6, 2, 2, 6, 8};
+    int resistance[10] = {8, 1, 5, 9, 5, 6, 8, 2, 3, 7};
     int voltage[10];
-    calcVolts(current,resistance,voltage);
-    dispVolts(current,resistance,voltage);
-   
-   
+    calcVolts(current, resistance, voltage);
+    dispVolts(current, resistance, voltage);
+
     return 0;
 }
 int calcVolts(int *current, int *resistance, int *voltage)
 {
-    for (int i = 0; i < 10; i++) {
-    voltage[i] = current[i] * resistance[i];
+    for (int i = 0; i < 10; i++)
+    {
+        voltage[i] = current[i] * resistance[i];
     }
 }
 void dispVolts(int *current, int *resistance, int *voltage)
@@ -26,7 +26,8 @@ void dispVolts(int *current, int *resistance, int *voltage)
     cout << "Current";
     cout << setw(15) << "Resistance";
     cout << setw(16) << "Voltage\n";
-    for (int i = 0; i < 10; i++) {
-    cout << current[i] << setw(15) << resistance[i] << setw(19) << voltage[i] << "\n";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << current[i] << setw(15) << resistance[i] << setw(19) << voltage[i] << "\n";
     }
 }
