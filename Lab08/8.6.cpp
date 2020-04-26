eb5aaabf1e4a8c4047400a240ab72d693d5bf84d
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-04-26 17:56:25 +1000

    8.6.cpp

--- Lab08/8.6.cpp
+++ Lab08/8.6.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
 int calcVolts(int *current, int *resistance, int *voltage);
-void dispVolts(int *voltage);
+void dispVolts(int *current, int *resistance, int *voltage);
 int main()
 {
     int current[10] = {2,5,10,9,3,6,2,2,6,8};
     int resistance[10] = {8,1,5,9,5,6,8,2,3,7};
     int voltage[10];
     calcVolts(current,resistance,voltage);
-    dispVolts(voltage);
+    dispVolts(current,resistance,voltage);
+   
+   
     return 0;
 }
 int calcVolts(int *current, int *resistance, int *voltage)
@@ -17,9 +21,12 @@ int calcVolts(int *current, int *resistance, int *voltage)
     voltage[i] = current[i] * resistance[i];
     }
 }
-void dispVolts(int *voltage)
+void dispVolts(int *current, int *resistance, int *voltage)
 {
+    cout << "Current";
+    cout << setw(15) << "Resistance";
+    cout << setw(16) << "Voltage\n";
     for (int i = 0; i < 10; i++) {
-    cout << voltage[i] << "\n";
+    cout << current[i] << setw(15) << resistance[i] << setw(19) << voltage[i] << "\n";
     }
 }
