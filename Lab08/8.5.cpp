43e9c80fa27ea06eb36f1e1db49128d8d887effc
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-04-26 16:06:01 +1000

    swap lab8

---
+++ Lab08/8.5.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+
+void reorder(int *a , int *b, int *c);
+
+int main()
+{
+    int a = 9;
+    int b = 6;
+    int c = 7;
+    cout << "Enter the value for A \n";
+    cin >> a;
+    cout << "Enter the value for B \n";
+    cin >> b;
+    cout << "Enter the value for C \n";  
+    cin >> c;
+
+    reorder(&a,&b,&c);
+    cout << "A: " << a << "\n";
+    cout << "B: " << b << "\n";
+    cout << "C: " << c << "\n";
+
+    return 0;
+
+}
+
+void reorder(int *a , int *b, int *c)
+{
+    int temp = 0;
+    if(*a>*b && *a>*c)
+    {
+        temp=*c;
+        *c=*a;
+        *a=temp;
+        if(*a>*b)
+        {
+            temp=*b;
+            *b=*a;
+            *a=temp;
+        }
+    }
+    if(*b>*a && *b>*c)
+    {
+        temp=*c;
+        *c=*b;
+        *b=temp;
+    }
+}
