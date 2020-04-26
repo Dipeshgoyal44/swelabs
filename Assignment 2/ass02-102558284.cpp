08b337254e64a00f5683393df8c0cfacf359827e
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-04-26 19:49:41 +1000

    assignment2finish

--- Assignment 2/ass02-102558284.cpp
+++ Assignment 2/ass02-102558284.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include <cstdio>
 //#include <cstdlib>
-
 using namespace std;
-
 int total1(int array[],int arraySize);
 int total2(int array[],int arraySize);
-
 int main()
 {
     int array[20];
@@ -15,7 +12,6 @@ int main()
     int count= 0;
     int sumTotal = 0;
     int checksum= 0;
-
     printf("Enter The Credit Card Number Below: \n");
     for (i = 0; i < 20; i++)
     {
@@ -35,24 +31,18 @@ int main()
         cout <<array[c];
     }
     cout << "\n";
-
-
 // SUM 1
     int sum1 = total1(array,count);
     cout << "Sum 1 is " << sum1 << "\n";
-
     // SUM 2 
     int sum2 = total2(array,count);
     cout << "Sum 2 is " <<  sum2 << "\n";
-
     // CheckSum
     sumTotal = sum1 + sum2;
     sumTotal = sumTotal * 9;
     checksum = sumTotal % 10;
-
     cout << "Check sum is " << checksum << "\n";
     cout << "The last digit of the credit card is " << array[count - 1] << "\n";
-
     if (array[count - 1] == checksum)
     {
         cout << "Check sum digit "<< checksum << " and the last digit " << array[count - 1] << " are the same: Valid credit card number. \n";
@@ -63,21 +53,22 @@ int main()
     }
     return 0;
 }
-
 int total1(int array[],int arraySize)
 {
     int total1=0;
     int i = 0;
     int x= 0;
     int sum= 0;
+    int temp[10];
+    int count = 0;
     cout<< "Numbers for sum1 are ";
-    for (i = 0; i < arraySize - 2; i = i + 2)
+    for (i = arraySize - 2; i >= 0; i = i - 2)
     {
-        cout<<  array[i] << " ";   
+        temp[count + 1] = array[i];
+        count++;
         x = array[i] * 2;
         if (x > 9)
         {
-
             x = x % 10;
             sum = 1 + x;
             total1 = total1 + sum;
@@ -87,25 +78,32 @@ int total1(int array[],int arraySize)
             total1 += array[i] * 2;
         }
     }
+    for (i = count; i > 0; i--)
+    {
+        cout << temp[i] << " ";
+
+
+    }
     cout <<  "\n";   
     return total1;
-
 }
-
 int total2(int array[],int arraySize)
 {
     int total2= 0;
     int i = 0;
+    int temp[10];
+    int count = 0;
     cout<< "Numbers for sum2 are ";
     for(i = arraySize - 3; i >= 0; i = i - 2)
     {
-        total2 = total2 + array[i];
-        cout<<  array[i] << " ";   
+        temp[count + 1] = array[i];
+        count++;
+        total2 = total2 + array[i]; 
         }
-        cout <<  "\n";   
+    for (i = count; i > 0; i--)
+    {
+        cout << temp[i] << " ";
+    }
+    cout <<  "\n";   
     return total2;
-    
 }
-
-
-
