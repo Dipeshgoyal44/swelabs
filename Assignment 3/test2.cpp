2ec1463bb64042eb178fa7e469d87e089f10d8bd
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-05-18 00:42:48 +1000

    ass03

--- Assignment 3/test2.cpp
+++ Assignment 3/test2.cpp
@@ -29,25 +29,19 @@ struct student_tag
 typedef struct student_tag Student_Tag;
 typedef Student_Tag *Student_TagPtr;
 
-Student_Tag *hptr;  
-Student_Tag *cptr; 
-Student_Tag *nptr;  
-
+Student_Tag *hptr;
+Student_Tag *cptr;
+Student_Tag *nptr;
 
 //function protypes
 void menu();
-student_tag read();
-student_tag *read_file();
+void read();
 void display_students();
 
-student_tag read() //readfile function
+void read() //readfile function
 {
-    hptr = NULL; //intialized to null
-    string name;
-    string id;
-    string coursename;
-    int units;
-    int marks[4];
+    double total;
+    hptr = nptr = NULL; //intialized to null
     string filename = "students.txt";
     ifstream inFile(filename);
 
@@ -57,123 +51,63 @@ student_tag read() //readfile function
         cout << "Please check that the file currently exists\n";
         exit(1);
     }
-    int i = 0;
     while (inFile.peek() != EOF) // until the end of the file
     {
-        double total = 0;
-        inFile >> name ;
-        inFile >> id;
-        inFile >> coursename;
-        inFile >> units;
-        for (int j = 0; j < units; j++) {
-        inFile >> marks[j];
-        total = total + marks[j];
+        total = 0;
+        cptr = new Student_Tag;
+        // Read the data into the new item
+        inFile >> cptr->student_info.name;
+        inFile >> cptr->student_info.id;
+        inFile >> cptr->course_info.course_name;
+        inFile >> cptr->course_info.no_of_units;
+        for (int j = 0; j < cptr->course_info.no_of_units; j++)
+        {
+            inFile >> cptr->course_info.marks[j];
+            total = total + cptr->course_info.marks[j];
         }
-        i++;
-    hptr = cptr = nptr; 
-    nptr = new Student_Tag;
-    nptr -> student_info.name = name;
-    //cout << nptr-> student_info.name << "\n"; //printing nptr
-    nptr->next = NULL; //nptr to next =null
-
-    hptr = cptr = nptr; 
-    nptr = new Student_Tag;
-    nptr -> student_info.id = id;
-    //cout << nptr-> student_info.id << "\n"; //printing nptr
-    nptr->next = NULL; //nptr to next =null
-    
-    hptr = cptr = nptr; 
-    nptr = new Student_Tag;
-    nptr -> course_info.course_name = coursename;
-    //cout << nptr-> course_info.course_name << "\n"; //printing nptr
-    nptr->next = NULL; //nptr to next =null
-
-    hptr = cptr = nptr; 
-    nptr = new Student_Tag;
-    nptr -> course_info.no_of_units = units;
-    //cout << nptr-> course_info.no_of_units << "\n"; //printing nptr
-    nptr->next = NULL; //nptr to next =null
-for (int j = 0; j < units; j++) {
-    hptr = cptr = nptr; 
-    nptr = new Student_Tag;
-    nptr -> course_info.marks[j] = marks[j];
-    //cout << nptr-> course_info.marks[j] << "\n"; //printing nptr
-    nptr->next = NULL; //nptr to next =null
+        double average =    total/ cptr->course_info.no_of_units;
+        cptr -> course_info.avg = average;
+        if (hptr == NULL)
+        {
+            // First item in the list. hptr pointing to first item
+            hptr = cptr;
+        }
+        else
+        {
+            // Not the first item, append it to the tail.
+            nptr->next = cptr;
+        }
+        nptr = cptr; // Move the tail pointer
     }
-    hptr = cptr = nptr; 
-    nptr = new Student_Tag;
-    nptr -> course_info.avg = total / units;
-    //cout << nptr-> course_info.avg << "\n"; //printing nptr
-    nptr->next = NULL; //nptr to next =null
-    }
-    
-    inFile.close();  //close file
+    cptr->next = NULL; //  last items next pointer set to NULL
+    inFile.close();    //close file
 }
-
-
 
 void display()
 {
-    while (hptr != NULL)
+    student_tag *ptr;
+    ptr = hptr;
+    while (ptr != NULL)
     {
-        cout << "The Student Name: " << hptr->student_info.name << "  \n";
-        cout << "The Student ID: " << hptr->student_info.id << "  \n";
-        // cout << "The course name: " << ptr->course_info.course_name << "  \n";
-        // cout << "Number of units " << ptr->course_info.no_of_units << "  \n";
-        // cout << "Marks recieved: \n";
-        // for (int j = 0; j < ptr->course_info.no_of_units; j++)
-        // {
-        //     cout << ptr->course_info.marks[j] << "  \n";
-        // }
-        // cout << "The marks average is : " << ptr->course_info.avg << "  \n";
-        // ptr = ptr->next;
+        cout << "The Student Name: " << ptr->student_info.name << "  \n";
+        cout << "The Student ID: " << ptr->student_info.id << "  \n";
+        cout << "The course name: " << ptr->course_info.course_name << "  \n";
+        cout << "Number of units " << ptr->course_info.no_of_units << "  \n";
+        cout << "Marks recieved: \n";
+        for (int j = 0; j < ptr->course_info.no_of_units; j++)
+        {
+            cout << ptr->course_info.marks[j] << "  \n";
+        }
+        cout << "Average : " << setprecision(2) << fixed << ptr->course_info.avg << " \n";
+        cout << "------------------------\n\n";
+        ptr = ptr->next;
     }
+    
 }
 
 int main()
 {
-    int array_size = 0;
     read();
-    cout << "The linked list is: ";
-   display();
+    display();
     return 0;
 }
-
-
-// student_tag *read_file() //readfile function
-// {
-//     int count = 0;
-//     double total = 0;
-//     string filename = "test.txt";
-//     ifstream inFile;
-
-//     inFile.open(filename.c_str());
-
-//     if (inFile.fail()) // if file doesnt open successfully
-//     {
-//         cout << "The File was not opened successfully\n";
-//         cout << "Please check that the file currently exists\n";
-//         return nullptr;
-//     }
-//     int i = 0;
-//     student_tag *tail = nullptr;
-//     student_tag *nptr = new student_tag;
-    
-//     while (inFile.peek() != EOF) // until the end of the file
-//     {
-//         if (tail)
-//         {
-//             tail->next = nptr;
-//         }
-//         else
-//         {
-//             hptr = nptr;
-//         }
-//         tail = nptr;
-//         nptr = new student_tag; // create space for the next one
-//     }
-//     // You created space for the one that failed to read. Delete it
-//     delete nptr;
-//     inFile.close(); //close file
-//     return hptr;
-// }
