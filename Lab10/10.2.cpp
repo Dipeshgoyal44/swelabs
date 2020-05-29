4fc21b41a048ff02837e834603ac370bc00d88f9
Dipeshgoyal44 <54257218+Dipeshgoyal44@users.noreply.github.com>
2020-05-29 15:25:17 +1000

    ass03

--- Lab10/10.2.cpp
+++ Lab10/10.2.cpp
@@ -3,48 +3,68 @@
 
 using namespace std;
 
-struct studentname {
+struct studentname
+{
     char letter;
-    struct studentname *next; 
-    };  
+    struct studentname *next;
+};
 typedef struct studentname STUDENTName;
 typedef STUDENTName *STUDENTNamePtr;
 //self referential structure
 
-int main() {
-    
-STUDENTName *hptr;  
-STUDENTName *cptr; 
-STUDENTName *nptr;  
+int main()
+{
 
-hptr = NULL; //intialized to null
+    STUDENTName *hptr;
+    STUDENTName *cptr;
+    STUDENTName *nptr;
+    STUDENTName *pptr;
 
-hptr = cptr = nptr; 
-nptr = new STUDENTName;
-nptr -> letter = 'A';
-cout << "Linked List\n";
-cout << nptr-> letter << "--> "; //printing nptr
-nptr->next = NULL; //nptr to next =null
+    hptr = NULL; //intialized to null
 
-nptr = new STUDENTName;
-nptr -> letter = 'G';
-cout << nptr-> letter << "--> "; //printing nptr
-nptr->next = NULL; //nptr to next =null
+    hptr = cptr = nptr;
+    nptr = new STUDENTName;
+    nptr->letter = 'A';
+    cout << "Linked List\n";
+    //cout << nptr-> letter << "--> "; //printing nptr
+    nptr->next = NULL; //nptr to next =null
+    hptr = nptr;
+    cptr = nptr;
 
-nptr = new STUDENTName;
-nptr -> letter = 'L';
-cout << nptr-> letter << "--> "; //printing nptr
-nptr->next = NULL; //nptr to next =null
+    nptr = new STUDENTName;
+    nptr->letter = 'G';
+    //cout << nptr-> letter << "--> "; //printing nptr
+    nptr->next = NULL; //nptr to next =null
+    hptr->next = nptr;
+    cptr = hptr->next;
 
-nptr = new STUDENTName;
-nptr -> letter = 'O';
-cout << nptr -> letter << "--> "; //printing nptr
-nptr->next = NULL; //nptr to next =null
+    nptr = new STUDENTName;
+    nptr->letter = 'L';
+    //cout << nptr-> letter << "--> "; //printing nptr
+    nptr->next = NULL; //nptr to next =null
+    hptr->next = nptr;
+    cptr = hptr->next;
 
-nptr = new STUDENTName;
-nptr -> letter = 'Y';
-cout << nptr-> letter << "\n"; //printing nptr
-nptr->next = NULL; //nptr to next =null
-cptr = hptr; 
-return 0;
+    nptr = new STUDENTName;
+    nptr->letter = 'O';
+    //cout << nptr -> letter << "--> "; //printing nptr
+    nptr->next = NULL; //nptr to next =null
+    hptr->next = nptr;
+    cptr = hptr->next;
+
+    nptr = new STUDENTName;
+    nptr->letter = 'Y';
+    //cout << nptr-> letter << "\n"; //printing nptr
+    nptr->next = NULL; //nptr to next =null
+    cptr->next = nptr;
+    cptr = hptr;
+
+    cout << "Linked List:\n";
+    while (cptr != NULL) //runs only when currentptr is not null
+    {
+
+        cout << cptr->letter << "\n"; // prints all the letters and number while the loop is running
+        cptr = cptr->next;
+    }
+    return 0;
 }
